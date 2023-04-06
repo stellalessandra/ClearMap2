cb2c97f9c2d4f8c8de25777b2497b2e456a4b024
stellalessandra <a.stella@fz-juelich.de>
2023-04-06 16:12:04 +0200

    added function to utils

--- ClearMap/Scripts/utils.py
+++ ClearMap/Scripts/utils.py
@@ -3,9 +3,12 @@ from yaml import Loader
 import os
 
 def list_subjects():
-    with open("ClearMap/Scripts/configfile.yaml", 'r') as stream:
-        config = yaml.load(stream, Loader=Loader)
-    
+    try:
+        with open("ClearMap/Scripts/configfile.yaml", 'r') as stream:
+            config = yaml.load(stream, Loader=Loader)
+    except FileNotFoundError:
+        with open("configfile.yaml", 'r') as stream:
+            config = yaml.load(stream, Loader=Loader) 
     user = config['user']
     experiment = config['experiment']
     experimental_group = config['experimental_group']
@@ -17,3 +20,14 @@ def list_subjects():
                 if os.path.isdir(os.path.join(data_directory, name))]
     
     return subjects
+
+def divide_in_exp_groups(list_subjects):
+    experimental_groups = {'Control':[],
+                          'Unfam':[],
+                          'Fam':[]}
+    for subject in list_subjects:
+        for exp_group in experimental_groups.keys():
+            if exp_group in subject:
+                experimental_groups[exp_group].append(subject)
+    return experimental_groups
+     
