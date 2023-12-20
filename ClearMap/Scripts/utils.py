e89e5d3aa2beaccdb34b87431d9b6b7be2d76fb7
User <a.stella@fz-juelich.de>
2023-12-20 14:41:17 +0100

    added analysis on black with father whole brain

--- ClearMap/Scripts/utils.py
+++ ClearMap/Scripts/utils.py
@@ -4,17 +4,17 @@ import os
 
 def list_subjects():
     try:
-        with open("ClearMap/Scripts/configfile.yaml", 'r') as stream:
+        with open("ClearMap/Scripts/configfile_ila.yaml", 'r') as stream:
             config = yaml.load(stream, Loader=Loader)
     except FileNotFoundError:
-        with open("configfile.yaml", 'r') as stream:
+        with open("configfile_ila.yaml", 'r') as stream:
             config = yaml.load(stream, Loader=Loader) 
     user = config['user']
     experiment = config['experiment']
     experimental_group = config['experimental_group']
     
-    data_directory = '/data01/' + user + '/Projects/' + experiment + '/' \
-                + experimental_group + '/'
+    data_directory = '/data/' + user + '/Projects/' + experiment + '/' \
+            + experimental_group + '/'
                 
     subjects = [name for name in os.listdir(data_directory) \
                 if os.path.isdir(os.path.join(data_directory, name))]
